e6b9d069448857aad41ef75002fd24fe035ef7b1
Leon28Phongretzka <lamperougelelouchanm@gmail.com>
2023-10-03 08:42:51 +0700

    2

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,42 +4,43 @@ import { compare } from 'bcrypt';
 import { sql } from '@vercel/postgres';
 
 const handler = NextAuth({
-  session: {
-    strategy: 'jwt',
-  },
-  pages: {
-    signIn: '/login',
-  },
-  providers: [
-    CredentialsProvider({
-      credentials: {
-        email: {},
-        password: {},
-      },
-      async authorize(credentials, req) {
-        //
-        const response = await sql`
-        SELECT * FROM users WHERE email=${credentials?.email}`;
-        const user = response.rows[0];
+    session: {
+      strategy: 'jwt',
+    },
+    pages: {
+      signIn: '/login',
+    },
+    providers: [
+      CredentialsProvider({
+        credentials: {
+          email: {},
+          password: {},
+        },
+        async authorize(credentials, req) {
+          //
+          const response = await sql`
+          SELECT * FROM users WHERE email=${credentials?.email}`;
+          const user = response.rows[0];
 
-        const passwordCorrect = await compare(
-          credentials?.password || '',
-          user.password
-        );
+          const passwordCorrect = await compare(
+            credentials?.password || '',
+            user.password
+          );
 
-        console.log({ passwordCorrect });
+          console.log({ passwordCorrect });
 
-        if (passwordCorrect) {
-          return {
-            id: user.id,
-            email: user.email,
-          };
-        }
+          if (passwordCorrect) {
+            return {
+              id: user.id,
+              email: user.email,
+            };
+          }
 
-        return null;
-      },
-    }),
-  ],
+          return null;
+        },
+      }),
+    ],
+    secret: process.env.NEXTAUTH_SECRET,
 });
 
 export { handler as GET, handler as POST };

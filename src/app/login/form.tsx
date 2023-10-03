d6588b322113639a31f987b5ae8f877179f1c754
Leon28Phongretzka <lamperougelelouchanm@gmail.com>
2023-10-03 10:27:43 +0700

    5

--- src/app/login/form.tsx
+++ src/app/login/form.tsx
@@ -1,12 +1,11 @@
-'use client';
-
+'use client'
 import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import { FormEvent } from 'react';
 
 export default function Form() {
-
   const router = useRouter();
+
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
@@ -22,6 +21,16 @@ export default function Form() {
       router.refresh();
     }
   };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      const form = e.currentTarget.form;
+      if (form) {
+        form.dispatchEvent(new Event('submit', { cancelable: true }));
+      }
+    }
+  };
+
   return (
     <form
       onSubmit={handleSubmit}
@@ -31,17 +40,18 @@ export default function Form() {
         name="email"
         className="px-2 py-4 rounded-md border border-black text-black"
         type="email"
-        placeholder='Email'
+        placeholder="Email"
       />
       <input
         name="password"
         className="px-2 py-4 rounded-md border border-black text-black"
         type="password"
-        placeholder='Password'
+        placeholder="Password"
+        onKeyDown={handleKeyDown}
       />
-      <button 
+      <button
         type="submit"
-        className='border border-black text-black bg-slate-400 px-2 py-4 hover:bg-slate-700 rounded-md'
+        className="border border-black text-black bg-slate-400 px-2 py-4 hover:bg-slate-700 rounded-md"
       >
         Login
       </button>
--- src/app/logout.tsx
+++ src/app/logout.tsx
@@ -5,7 +5,7 @@ export default function Logout() {
   return (
     <button
       onClick = {() => signOut()}
-      className="border rounded-md border-black bg-slate-400"
+      className="px-8 py-4 border rounded-md border-black bg-slate-400 hover:bg-slate-600"
     >
       Logout
     </button>
